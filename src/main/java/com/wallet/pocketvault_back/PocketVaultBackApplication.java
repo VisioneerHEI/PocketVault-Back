3f7c652ed033733a4b99ed6aab2673273f2e998e
Astna17 <hei.antsa.17@gmail.com>
2024-02-05 16:13:32 +0300

    first commit

---
+++ src/main/java/com/wallet/pocketvault_back/PocketVaultBackApplication.java
@@ -0,0 +1,13 @@
+package com.wallet.pocketvault_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocketVaultBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PocketVaultBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wallet/pocketvault_back/PocketVaultBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wallet.pocketvault_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocketVaultBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
