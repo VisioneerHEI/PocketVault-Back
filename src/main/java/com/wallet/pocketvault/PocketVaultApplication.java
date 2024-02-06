76c3a2176dc9bfe9925c3d5fa49d0004bb8e387b
Astna17 <hei.antsa.17@gmail.com>
2024-02-06 11:30:37 +0300

    first commit

---
+++ src/main/java/com/wallet/pocketvault/PocketVaultApplication.java
@@ -0,0 +1,13 @@
+package com.wallet.pocketvault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocketVaultApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PocketVaultApplication.class, args);
+    }
+
+}
