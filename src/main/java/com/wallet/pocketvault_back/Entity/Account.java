23450e6906ff25aa7a09b12e2725c23d7761879b
Astna17 <hei.antsa.17@gmail.com>
2024-02-17 17:02:49 +0300

    feat: add class Account

--- src/main/java/com/wallet/pocketvault_back/Entity/Account.java
+++ src/main/java/com/wallet/pocketvault_back/Entity/Account.java
@@ -1,4 +1,19 @@
 package com.wallet.pocketvault_back.Entity;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
 public class Account {
+    private int accountId;
+    private String name;
+    private double balance;
+    private String accountType;
+    private Currency id_Currency;
 }
