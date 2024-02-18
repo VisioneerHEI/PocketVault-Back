92d8030a808c935ab75cdc19cf0fd9bd1f2c8262
MiiJack <f.rama723@gmail.com>
2024-02-18 22:57:15 +0300

    feat: implemented currency exchange rate endpoint

---
+++ src/main/java/visioneerHEI/pocketvault_back/models/ExchangeRateResponse.java
@@ -0,0 +1,14 @@
+package visioneerHEI.pocketvault_back.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Map;
+
+@Getter
+@Setter
+public class ExchangeRateResponse {
+    private String result;
+    private String base_code;
+    private Map<String, Double> conversion_rates;
+}
