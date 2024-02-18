4aa02b5f7063cd936932530837f52ffcc5829bce
Astna17 <hei.antsa.17@gmail.com>
2024-02-18 07:33:55 +0300

    feat: create enpoint for transasction history

--- src/main/java/com/wallet/pocketvault_back/Entity/TransferHistory.java
+++ src/main/java/com/wallet/pocketvault_back/Entity/TransferHistory.java
@@ -1,14 +1,20 @@
 package com.wallet.pocketvault_back.Entity;
 
+import lombok.*;
+
 import java.sql.Timestamp;
 import java.util.List;
 
-public class TransferHistoryEntry {
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class TransferHistory {
     private int id_historyEntry;
+    private User users;
     private int debitTransactionId;
     private int creditTransactionId;
     private double transferAmount;
     private Timestamp transferDate;
-    private List <Transaction> debitTransactions;
-    private List <Transaction> creditTransactions;
 }
